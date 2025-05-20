f2797f82b2f8352d6cfffd538a2bfeb1da91df3d
Kevin Wang <kevwang@f6r10s16.clusters.42paris.fr>
2025-05-20 16:49:55 +0200

    up

---
+++ v02/client.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevwang <kevwang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 10:20:40 by kevwang           #+#    #+#             */
+/*   Updated: 2025/05/19 10:20:42 by kevwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_base(int c, int i, char *newstr)
+{
+	if (c >= 2)
+	{
+		ft_base(c / 2, i - 1, newstr);
+	}
+	newstr[i] = "01"[c % 2];
+}
+
+char	*ft_call_bit(unsigned char c)
+{	
+	char	*bit;
+	int		i;
+
+	i = 0;
+	bit = malloc(sizeof(char) * (8 + 1));
+	if (!bit)
+		return (NULL);
+	while (i < 8)
+	{
+		bit[i] = '0';
+		i++;
+	}
+	ft_base(c, 7, bit);
+	bit[8] = '\0';
+	return (bit);
+}
+
+void	ft_send_signal(pid_t pid, char *str)
+{
+	int	i;
+	int	killtest;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '0')
+			killtest = kill(pid, SIGUSR1);
+		else if (str[i] == '1')
+			killtest = kill(pid, SIGUSR2);
+		i++;
+		usleep(10);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t	pidserver;
+	char	*bit;
+	int		i;
+
+	i = 0;
+	if (argc >= 3)
+	{
+		pidserver = ft_atoi(argv[1]);
+		printf("%d\n", pidserver);
+		// len = ft_strlen(argv[2][i]);
+		while (argv[2][i])
+		{
+			bit = ft_call_bit(argv[2][i]);
+			ft_send_signal(pidserver, bit);
+			free(bit);
+			i++;
+		}
+	}
+}
---
+++ v02/server.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevwang <kevwang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 10:20:46 by kevwang           #+#    #+#             */
+/*   Updated: 2025/05/19 10:20:48 by kevwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	ft_convert(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == '0')
+			i++;
+		else if (str[i] == '1')
+		{
+			count = count + ft_power(2, 7 - i);
+			i++;
+		}
+	}
+	return (count);
+}
+
+void ft_print(int signum)
+{
+	int count;
+	static char str[500];
+	static char bit[9];
+	static int i = 0;
+	static int j = 0;
+	// if (!str)
+	// {
+	// 	str = ft_calloc(sizeof(char) * 500);
+	// 	if (!str)
+	// 		return ;
+	// }
+	if (signum == 10)
+		bit[i] = '0';
+	else if (signum == 12)
+		bit[i] = '1';
+	i++;
+	bit[8] = '\0';
+	// printf("signum :%d\n", signum);
+	// printf("i :%d bit :%s\n",i, bit);
+	if (i == 8)
+	{
+		// printf("%s\n", bit);
+		count = ft_convert(bit);
+		// printf("%c\n", (char)count);
+		str[j] = (char)count;
+		// printf("%c\n", str[j]);
+		// ft_pustr(str);
+		j++;
+		// ft_putchar(count);
+		count = 0;
+		while (i > 0)
+		{
+			bit[i] = '0';
+			i--;
+		}
+		// printf("i :%d bit :%s\n",i, bit);
+		if (str[j] == '\0')
+		{
+			ft_pustr(str);
+			j = 0;
+		}
+	}
+	
+}
+
+int main()
+{
+	// signal(SIGUSR1, ft_print);
+	// signal(SIGUSR2, ft_print);
+
+	pid_t pidserver = getpid();
+	printf("SERVER PID : %d\n", pidserver);
+	while (1)
+	{
+		signal(SIGUSR1, ft_print);
+		signal(SIGUSR2, ft_print);
+	}
+}
